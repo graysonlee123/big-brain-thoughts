a7c07da76dfc430e98c93884150726e949fcc83e
Grayson Gantek <graysonleegantek@gmail.com>
2023-03-26 21:02:16 -0400

    converted file to typescript

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,8 +9,8 @@ export const authOptions = {
   }),
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      clientId: process.env.DISCORD_CLIENT_ID ?? '',
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
     }),
   ],
 }
