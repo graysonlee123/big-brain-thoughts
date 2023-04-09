7a60fc185c0291bb006efbaf309863d5034bc8a6
Grayson Gantek <graysonleegantek@gmail.com>
2023-04-09 15:42:31 -0400

    added user id pages and api route

---
+++ pages/api/quotes/user/[id].ts
@@ -0,0 +1,95 @@
+import { NextApiHandler } from 'next'
+import { getServerSession } from 'next-auth'
+import { ObjectId } from 'mongodb'
+import authOptions from '@lib/authOptions'
+import createApiResponse from '@lib/createApiResponse'
+import clientPromise from '@lib/db'
+
+const dbName = process.env.MONGOD_DB_NAME
+
+if (undefined === dbName) {
+  throw new Error('Invalid / Missing environment variable: "MONGOD_DB_NAME"')
+}
+
+const handler: NextApiHandler = async (req, res) => {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (null === session) {
+    res.status(401).json(createApiResponse(false, null, 'No session found.'))
+    return
+  }
+
+  if (req.method === 'GET') {
+    try {
+      /** Get the DB connection. */
+      const client = await clientPromise
+      const db = await client.db(dbName)
+
+      /** Grab the quotes collection. */
+      const quotesCollection = db.collection('quotes')
+
+      /** Get the quotes that contain the user id from the request query. */
+      const id = req.query.id as string
+      const conversations = await quotesCollection
+        .aggregate([
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'submitter_id',
+              foreignField: '_id',
+              as: 'submitter_data',
+            },
+          },
+          {
+            $unwind: '$submitter_data',
+          },
+          {
+            $unwind: '$quotes',
+          },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'quotes.speaker_id',
+              foreignField: '_id',
+              as: 'quotes.speaker_data',
+            },
+          },
+          {
+            $unwind: '$quotes.speaker_data',
+          },
+          {
+            $match: {
+              'quotes.speaker_id': new ObjectId(id),
+            },
+          },
+          {
+            $group: {
+              _id: '$_id',
+              submitter_data: { $first: '$submitter_data' },
+              date_time: { $first: '$date_time' },
+              quotes: { $push: '$quotes' },
+            },
+          },
+          {
+            $sort: { date_time: -1 },
+          },
+        ])
+        .toArray()
+
+      res.json(createApiResponse(true, conversations, 'Found quotes based on that user.'))
+    } catch (error) {
+      console.error(error)
+      res
+        .status(500)
+        .json(createApiResponse(false, error, 'There was an issue getting the quotes.'))
+    }
+    return
+  } else {
+    res
+      .status(404)
+      .json(createApiResponse(false, null, `There is no ${req.method} method for this route.`))
+    return
+  }
+}
+
+export default handler
---
+++ pages/user/[id].tsx
@@ -0,0 +1,48 @@
+import { GetServerSideProps, NextPage } from 'next'
+import { getServerSession } from 'next-auth'
+import AuthedLayout from '@components/AuthedLayout/AuthedLayout'
+import ConvoList from '@components/ConvoList'
+import authOptions from '@lib/authOptions'
+
+interface UserIdPageProps {
+  convos: ExpandedConversation[]
+}
+
+const UserIdPage: NextPage<UserIdPageProps> = ({ convos }) => {
+  return (
+    <AuthedLayout>
+      <ConvoList convos={convos} />
+    </AuthedLayout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
+  const session = await getServerSession(req, res, authOptions)
+  const baseURL = process.env.NEXTAUTH_URL
+  const id = query.id
+  let convos: ExpandedConversation[] = []
+
+  if (!baseURL) console.warn('No `NEXTAUTH_URL` environment variable!')
+
+  if (session) {
+    try {
+      const res = await fetch(`${baseURL}/api/quotes/user/${id}`, {
+        headers: { Cookie: req.headers.cookie ?? '' },
+      })
+      const json: APIResponse<ExpandedConversation[]> = await res.json()
+
+      convos = json.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return {
+    props: {
+      convos,
+      session,
+    },
+  }
+}
+
+export default UserIdPage
