4ab627cc5c74453e2c29e142b07e6781d4725e98
Grayson Gantek <graysonleegantek@gmail.com>
2023-04-11 22:08:36 -0400

    update user link to accept link props

--- components/UserLink/UserLink.tsx
+++ components/UserLink/UserLink.tsx
@@ -1,10 +1,16 @@
-import { Link } from '@mui/material'
+import { Link, LinkProps } from '@mui/material'
 import { User } from 'next-auth'
 
-interface UserLinkProps {
+interface UserLinkProps extends Omit<LinkProps, 'href'> {
   user: User
 }
 
-export default function UserLink({ user }: UserLinkProps) {
-  return <Link href={`/user/${user._id}`}>{user.name ?? 'Unknown'}</Link>
+export default function UserLink({ user, ...props }: UserLinkProps) {
+  return (
+    <Link {...props} href={`/user/${user._id}`}>
+      {user.name ?? 'Unknown'}
+    </Link>
+  )
 }
+
+UserLink.muiName = 'Link'
