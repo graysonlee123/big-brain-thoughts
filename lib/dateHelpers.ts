ef9da1ed674f9e5a32d1b260a32abf3f28f54274
Grayson Gantek <graysonleegantek@gmail.com>
2023-04-16 15:43:14 -0400

    changed date formatter

--- lib/dateHelpers.ts
+++ lib/dateHelpers.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
 
 export function displayDate(date: string | number | Date | dayjs.Dayjs | null | undefined) {
-  return dayjs(date).format('MMMM D, YYYY')
+  return dayjs(date).format('MM/DD/YY')
 }
