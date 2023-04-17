632caa2a36bb24a6226b9e6361e1108a56d5b119
Grayson Gantek <graysonleegantek@gmail.com>
2023-04-16 21:31:50 -0400

    added a backup for when there are no conversations found in the list

--- components/ConvoList/ConvoList.tsx
+++ components/ConvoList/ConvoList.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { User } from 'next-auth'
-import { Box, Stack, Typography } from '@mui/material'
+import { Alert, AlertTitle, Box, Container, Stack, Typography } from '@mui/material'
 import Masonry from '@mui/lab/Masonry'
 import useSortedConvos from '@hooks/useSortedConvos'
 import Convo from '@components/Convo'
@@ -13,37 +13,47 @@ interface ConvoListProps {
 
 export default function ConvoList({ convos, user }: ConvoListProps) {
   const sortedConvos = useSortedConvos(convos)
+  const asArray = Array.from(sortedConvos)
 
   return (
     <>
       {user && <UserHeader user={user} />}
-      <Stack gap={8}>
-        {Array.from(sortedConvos).map(([year, convos]) => (
-          <Box key={year}>
-            <Stack gap={2}>
-              <Box sx={{ textAlign: 'center' }}>
-                <Typography variant="h3">{year}</Typography>
-                <Typography variant="subtitle2">{convos.length} quotes</Typography>
-              </Box>
-              <Box>
-                <Masonry
-                  columns={{ sm: 1, md: 2, lg: 3 }}
-                  spacing={2}
-                  defaultColumns={3}
-                  defaultSpacing={2}
-                  sx={{ m: 0 }}
-                >
-                  {convos.map((convo) => (
-                    <Fragment key={convo._id}>
-                      <Convo convo={convo} />
-                    </Fragment>
-                  ))}
-                </Masonry>
-              </Box>
-            </Stack>
-          </Box>
-        ))}
-      </Stack>
+      {asArray.length === 0 ? (
+        <Container maxWidth="sm">
+          <Alert severity="info" variant="filled">
+            <AlertTitle>Nothing found</AlertTitle>
+            Sorry, no quotes were found.
+          </Alert>
+        </Container>
+      ) : (
+        <Stack gap={8}>
+          {asArray.map(([year, convos]) => (
+            <Box key={year}>
+              <Stack gap={2}>
+                <Box sx={{ textAlign: 'center' }}>
+                  <Typography variant="h3">{year}</Typography>
+                  <Typography variant="subtitle2">{convos.length} quotes</Typography>
+                </Box>
+                <Box>
+                  <Masonry
+                    columns={{ sm: 1, md: 2, lg: 3 }}
+                    spacing={2}
+                    defaultColumns={3}
+                    defaultSpacing={2}
+                    sx={{ m: 0 }}
+                  >
+                    {convos.map((convo) => (
+                      <Fragment key={convo._id}>
+                        <Convo convo={convo} />
+                      </Fragment>
+                    ))}
+                  </Masonry>
+                </Box>
+              </Stack>
+            </Box>
+          ))}
+        </Stack>
+      )}
     </>
   )
 }
